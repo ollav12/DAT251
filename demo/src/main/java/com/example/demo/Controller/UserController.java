33891eb2896a0655203f84865ba51eb76625dd58
ollav12 <90145593+ollav12@users.noreply.github.com>
2025-02-19 11:50:33 +0100

    Started on user model

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -1,4 +1,18 @@
 package com.example.demo.Controller;
 
+//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// @CrossOrigin(origins = "?")
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/{username}")
+    public String getUser(@PathVariable String username) {
+        return "Hello " + username;
+    }
 }
--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -1,4 +1,52 @@
 package com.example.demo.Model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+//@Entity
+//@Table(name = "users")
 public class User {
+
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    // @Column(unique = true)
+    private String username;
+    private String email;
+    private String password;
+    private int points;
+
+    public User(String firstName, String lastName, String username, String email, String password, int points) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.points = points;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getPoints() {
+        return points;
+    }
 }
