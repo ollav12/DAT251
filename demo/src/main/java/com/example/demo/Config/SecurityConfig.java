57ad009165e6c06a45039356aae10d36aad00283
h599025 <89259607+h599025@users.noreply.github.com>
2025-02-20 10:46:37 +0100

    Security funker

--- demo/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -2,13 +2,31 @@ package com.example.demo.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults()) // Enables CORS
+                .csrf(csrf -> csrf.disable()) // Disables CSRF protection
+                .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/*", "/auth/*").permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
     }
 }
