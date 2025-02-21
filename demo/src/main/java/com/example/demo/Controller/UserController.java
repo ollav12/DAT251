6cf844a52bf40d9fd94ba4269a585aebe22130a9
602822 <89077126+602822@users.noreply.github.com>
2025-02-21 14:36:13 +0100

    added CRUD methods for User

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -3,8 +3,12 @@ package com.example.demo.Controller;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.demo.Model.User;
 import com.example.demo.Service.UserServiceImpl;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +34,20 @@ public class UserController {
     public List<User> getUsers() {
         return userService.getAllUsers();
     }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable long id) {
+        return userService.getUser(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@RequestBody User updatedUser, @PathVariable long id) {
+        userService.updateUser(updatedUser, id);
+    }
+
 }
