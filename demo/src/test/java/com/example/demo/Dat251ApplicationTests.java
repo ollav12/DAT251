31f440fd1962121400754f26a8aedcf7eda24197
602822 <89077126+602822@users.noreply.github.com>
2025-02-26 11:55:41 +0100

    added tests for updating and deleting a user

--- demo/src/test/java/com/example/demo/Dat251ApplicationTests.java
+++ demo/src/test/java/com/example/demo/Dat251ApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 
 import com.example.demo.Model.User;
@@ -27,6 +29,9 @@ class Dat251ApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 	private User createTestUser() {
 		User user = new User();
 		user.setUsername("Test");
@@ -38,6 +43,17 @@ class Dat251ApplicationTests {
 		return user;
 	}
 
+	private User createTestUser2() {
+		User user = new User();
+		user.setUsername("Test2"); // new username
+		user.setPassword("password123");
+		user.setEmail("test@gmail.com");
+		user.setFirstName("MyName");
+		user.setLastName("MyLastName");
+		user.setPoints(200); // +100 points
+		return user;
+	}
+
 	@Test
 	void contextLoads() {
 	}
@@ -46,19 +62,60 @@ class Dat251ApplicationTests {
 	void testCreatingAndRetrievingUser() {
 		User user = createTestUser();
 
-		// Tests the POST createUser method
+		// Executes the POST createUser method
 		ResponseEntity<User> response = restTemplate.postForEntity("/users", user, User.class);
 		User registeredUser = response.getBody();
 		assertNotNull(registeredUser);
 		assertEquals("Test", registeredUser.getUsername());
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
-		// Tests the GET getUser(long id) method
+		// Executes the GET getUser method
 		ResponseEntity<User> getResponse = restTemplate.getForEntity("/users/" + registeredUser.getId(), User.class);
 		User retrievedUser = getResponse.getBody();
 		assertNotNull(retrievedUser);
 		assertEquals(HttpStatus.OK, getResponse.getStatusCode());
 		assertEquals("Test", retrievedUser.getUsername());
+	}
+
+	@Test
+	void testDeletingUser() {
+		User user = createTestUser();
+		ResponseEntity<User> response = restTemplate.postForEntity("/users", user, User.class);
+		User registeredUser = response.getBody();
+		assertNotNull(registeredUser);
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+		// Executes the DELETE deleteUser method
+		restTemplate.delete("/users/" + registeredUser.getId());
+		ResponseEntity<User> getResponse = restTemplate.getForEntity("/users" + registeredUser.getId(), User.class);
+		assertEquals(HttpStatus.NOT_FOUND, getResponse.getStatusCode());
+	}
+
+	@Test
+	void testUpdatingUser() {
+		User user = createTestUser();
+		User user2 = createTestUser2();
+
+		ResponseEntity<User> response = restTemplate.postForEntity("/users", user, User.class);
+		User registeredUser = response.getBody();
+		assertNotNull(registeredUser);
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+		// Executes the PUT updateUser method
+		restTemplate.put("/users/" + registeredUser.getId(), user2);
+
+		ResponseEntity<User> getResponse = restTemplate.getForEntity("/users/" + registeredUser.getId(), User.class);
+		assertEquals(HttpStatus.OK, getResponse.getStatusCode());
+		User updatedUser = getResponse.getBody();
+
+		assertNotNull(updatedUser);
+		assertEquals("Test2", updatedUser.getUsername());
+		assertTrue(passwordEncoder.matches("password123", updatedUser.getPassword()));
+		assertEquals("test@gmail.com", updatedUser.getEmail());
+		assertEquals("MyName", updatedUser.getFirstName());
+		assertEquals("MyLastName", updatedUser.getLastName());
+		assertEquals(200, updatedUser.getPoints());
+
 	}
 
 }
